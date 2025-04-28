ce64a6d63d063ad7b1615b671be1c6d9650c1e44
juliaxgrzyll <julia.grzyll@gmail.com>
2025-04-28 07:37:15 +0200

    Add files via upload

---
+++ 3_delivery_country.sql
@@ -0,0 +1,14 @@
+/*3 countries of delivery, the largest nr of customers
+ nr of orders between 160 and 200 */
+select 
+ 	delivery_country
+ 	,count(distinct customer_id) as nr_of_customers
+ 	,count(order_id)          	 as nr_of_orders
+from my_orders
+group by 1
+having count(order_id) between 160 and 200
+order by count(distinct customer_id) desc
+limit 3
+
+
+
